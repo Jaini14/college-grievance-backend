83898b814ef1357a638f2569568cb4757536fb1b
jaini <pateljaini9724@gmail.com>
2026-05-19 19:54:59 +0530

    fix render decouple error

--- backend/settings.py
+++ backend/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os
-from decouple import config
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -25,14 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
-
-DEBUG = config("DEBUG", default=False, cast=bool)
-
-ALLOWED_HOSTS = config(
+SECRET_KEY = os.getenv("SECRET_KEY")
+DEBUG = os.getenv("DEBUG") == "True"
+ALLOWED_HOSTS = os.getenv(
     "ALLOWED_HOSTS",
-    cast=lambda v: [s.strip() for s in v.split(",")]
-)
+    ""
+).split(",")
 
 # Application definition
 INSTALLED_APPS = [
